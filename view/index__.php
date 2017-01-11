0b0da73041106b22337bc6dd8bd7b632cd10c8e1
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-01-12 00:46:38 +0700

    Update Hooks

--- view/index__.php
+++ view/index__.php
@@ -9,18 +9,16 @@ function fn_view_set($path) {
     }
 }
 
-function fn_view_get($data) {
+function fn_view_get($view) {
     global $language;
-    if (isset($data['view'])) {
-        $i = $data['view'];
-        $data['view'] = $i . ' ' . $language->page_view[$i === 1 ? 0 : 1];
-    } else {
-        $data['view'] = '0 ' . $language->page_view[1];
+    if ($view !== null) {
+        $i = (int) $view;
+        return $view . ' ' . $language->page_view[$i === 1 ? 0 : 1];
     }
-    return $data;
+    return '0 ' . $language->page_view[1];
 }
 
-Hook::set('page.output', 'fn_view_get');
+Hook::set('page.view', 'fn_view_get');
 
 // is online …
 if (strpos(X . '127.0.0.1' . X . '::1' . X, X . $_SERVER['REMOTE_ADDR'] . X) === false) {
